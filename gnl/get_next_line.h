5b705c0bd1d2db0e592db88db678b5ebf2cff892
Dan Oliver Monells <daoliver@car9s1.42barcelona.com>
2023-10-04 17:13:17 +0200

    testing

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,18 +6,28 @@
 /*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 16:12:41 by daoliver          #+#    #+#             */
-/*   Updated: 2023/10/04 16:14:46 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/10/04 17:12:56 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#indef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 50
+# endif
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <limits.h>
+
+/* PROTOTYPE GET_NEXT_LINE */
+
+char	*get_next_line(int fd);
+
+/* ADITIONAL FUNCTIONS */
 
 
 
