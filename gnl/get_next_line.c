37f2b555f1af97d4b23cbee533a1c6ad8316927e
Dan Oliver Monells <daoliver@car9s1.42barcelona.com>
2023-10-04 17:04:18 +0200

    null

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,10 +6,17 @@
 /*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 16:12:12 by daoliver          #+#    #+#             */
-/*   Updated: 2023/10/04 16:12:35 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/10/04 17:03:46 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	static char	*storage = NULL;
+	char		*line;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+}
