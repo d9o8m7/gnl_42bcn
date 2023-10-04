1d6d62b84513ca2e5b3a148a2a2d551eb20e1383
Dan Oliver Monells <daoliver@car9s1.42barcelona.com>
2023-10-04 16:51:02 +0200

    practice

---
+++ gnl/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 16:12:41 by daoliver          #+#    #+#             */
+/*   Updated: 2023/10/04 16:14:46 by daoliver         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#indef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <limits.h>
+
+
+
+#endif
