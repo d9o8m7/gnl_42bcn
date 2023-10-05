76b7e53cff0cbf000d5e757e182613009c506f00
Dan Oliver Monells <daoliver@car9s4.42barcelona.com>
2023-10-05 18:26:45 +0200

    ‘practice’
    Signed-off-by: Dan Oliver Monells <daoliver@car9s4.42barcelona.com>

--- gnl_practice/get_next_line.c
+++ gnl_practice/get_next_line.c
@@ -1,6 +1,48 @@
 
 #include "get_next_line.h"
 
+	//Get my line
+char	*get_line(t_list *list)
+{
+	int		str_len;
+	char	*next_str;
+
+	if (NULL == list)
+		return (NULL);
+
+	//Count CHAR till '\n'
+	//So i know how much malloc
+	str_len = len_to_newline(list);
+	next_str = malloc(str_len + 1);
+	if (NULL == next_str)
+		return (NULL);
+
+	//Copy string in buffer and return it
+	copy_str(list, next_str);
+	return (next_str);
+}
+
+	//Append one Node to the end of LIST
+void	append(t_list **list, char *buf)
+{
+	t_list	*new_node;
+	t_list	*last_node;
+
+	last_node = find_last_node(*list);
+	new_node = malloc(sizeof(t_list));
+	if (NULL == new_node)
+		return ;
+	// If list is empty
+	// If null == *lis
+	if (NULL == last_node)
+		*list = new_node;
+	else
+		last_node->next = new_node;
+
+	new_node->str_buf = buf;
+	new_node->next = NULL;
+}
+
 void	create_list(t_list **list, int fd)
 {
 	//Necessary to place \0 -> "string\0"
