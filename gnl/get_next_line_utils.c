e4fe5a297bf81f0fc5d545404a162fac85191d4c
Dan Oliver Monells <daoliver@car9s4.42barcelona.com>
2023-10-05 12:55:52 +0200

    utils strlen strchr

--- gnl/get_next_line_utils.c
+++ gnl/get_next_line_utils.c
@@ -6,8 +6,30 @@
 /*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 16:14:54 by daoliver          #+#    #+#             */
-/*   Updated: 2023/10/04 16:15:12 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/10/05 12:55:20 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	if (s[i] == '\0')
+		return (NULL);
+	return (&s[i]);
+}
