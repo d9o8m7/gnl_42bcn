9417cc9a7020e2ebe082fcf32b3e7e1a430d316f
Dan Oliver Monells <daoliver@car9s4.42barcelona.com>
2023-10-05 18:38:14 +0200

    ‘practice’
    Signed-off-by: Dan Oliver Monells <daoliver@car9s4.42barcelona.com>

--- gnl_practice/get_next_line_utils.c
+++ gnl_practice/get_next_line_utils.c
@@ -1,5 +1,31 @@
 #include "get_next_line.h"
 
+void	copy_str(t_list *list, char *str)
+{
+	int	i;
+	int	k;
+
+	if (NULL == list)
+		return ;
+	k = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				str[k++] = '\n';
+				str[k] = '\0';
+				return ;
+			}
+			str[k++] = list->str_buf[i++];
+		}
+		list = list->next;
+	}
+	str[k] = '\0';
+}
+
 int	len_to_newline(t_list *list)
 {
 	int	i;
