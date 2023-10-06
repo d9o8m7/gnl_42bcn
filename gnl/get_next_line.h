65be224fb0f45edb48eb458a9270f60c38aa58e9
Dan Oliver Monells <daoliver@car9s4.42barcelona.com>
2023-10-06 15:39:08 +0200

    ‘practice’
    Signed-off-by: Dan Oliver Monells <daoliver@car9s4.42barcelona.com>

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/04 16:12:41 by daoliver          #+#    #+#             */
-/*   Updated: 2023/10/04 17:12:56 by daoliver         ###   ########.fr       */
+/*   Created: 2023/10/06 15:17:28 by daoliver          #+#    #+#             */
+/*   Updated: 2023/10/06 15:27:30 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,32 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 50
+#  define BUFFER_SIZE 50
 # endif
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <limits.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
 
-/* PROTOTYPE GET_NEXT_LINE */
+/* PROTOTYPE */
 
 char	*get_next_line(int fd);
 
 /* ADITIONAL FUNCTIONS */
 
+size_t	ft_strlen(char *s);
+size_t	ft_strlcpy(char *dest, char *src, size_t destsize);
+char	*fr_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
 
+/* MAIN FUNCTIONS */
 
+char	*read_storage(int fd, char *storage);
+char	*extract_storage(char *storage);
+char	*clean_storage(char *storage);
+char	*free_storage(char *storage);
 #endif
